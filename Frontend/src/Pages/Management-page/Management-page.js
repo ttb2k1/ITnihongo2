04cf7654abf7a4aea6d36a9d0297053240f90697
Đoàn Ngọc Châu Anh <doanngocchauanh@gmail.com>
2022-11-17 02:08:02 +0700

    Update Management Page

--- Frontend/src/Pages/Management-page/Management-page.js
+++ Frontend/src/Pages/Management-page/Management-page.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import './Management-page.css';
 import Header from "../../Components/Header-Management-page/Header";
-import Table from "./components/Table/Table"
 import WebcamCapture from "./components/Webcam/Webcam";
 import CarInfo from "./components/CarInfo/CarInfo";
 const ManagementPage = () => {
+    const [rotate ,setRotate] = useState(false);
+    console.log("retate situatus : ",rotate);
+    rotate?document.body.style.overflow="hidden":document.body.style.overflow="auto";
     return (
         <div>
             <Header/>
-            <div className="Mainpage">
+            <div className="main-page">
                 <WebcamCapture/>
                 <CarInfo/>
-                <label className="history-car"> HISTORY </label>
-                <Table/>
             </div>
         </div>
     )
