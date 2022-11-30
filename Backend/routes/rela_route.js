31a63e20a29d6438da2e121322106ad205e0936f
Minh Hieu <oppahd96@gmail.com>
2022-11-30 18:24:38 +0700

    delete backend

--- Backend/routes/rela_route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const rela_controller = require("../controllers/rela_controller")
-
-router.get("/getlist",rela_controller.getList)
-
-module.exports = router
