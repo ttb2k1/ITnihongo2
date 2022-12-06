fab3237a5da1bc24404d2ecc3ce062abcb14c68e
unknown <ttb2k1@gmail.com>
2022-12-06 16:59:28 +0700

    Update Call API

--- Backend/app.js
+++ Backend/app.js
@@ -9,12 +9,29 @@ var passport = require("passport")
 var flash = require('connect-flash');
 require("dotenv").config()
 
-var user_route = require('./routes/user_route')
-var account_route = require('./routes/account_route')
+var hist_route = require('./routes/hist_route');
+var user_route = require('./routes/user_route');
+var account_route = require('./routes/account_route');
+var pos_route = require('./routes/pos_route');
+var veh_route = require('./routes/veh_route');
+var rela_route = require('./routes/rela_route');
 var session_connection = require("./config/session_database")
 
 
-var app = express();
+var app = express()
+
+
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://localhost:3000']
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, Content-Type, X-Auth-Token, X-Requested-With, Accept')
+  res.header('Access-Control-Allow-Credentials', true);
+  next()
+})
 
 
 var session_store = new MySQLStore({
@@ -53,7 +70,11 @@ app.use(passport.session())
 require('./config/passport')
 
 
-app.use('/user', user_route)
-app.use('/account', account_route)
+app.use('/history', hist_route);
+app.use('/user', user_route);
+app.use('/account', account_route);
+app.use('/position', pos_route);
+app.use('/relative', rela_route);
+app.use('/vehicle', veh_route);
 
 module.exports = app;
---
+++ Frontend/src/Pages/Account-management-page/components/TableAccount/TableAccount.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import './TableAccount.css';
+import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai';
+import ManagementService from '../../../../Services/ManagementService';
+
+const TableAccount = () => {
+  const [data, setData] = useState([])
+
+
+  useEffect(() => {
+    try {
+      ManagementService.getAllUser().then((res) => {
+        setData(res.data.data)
+        console.log(res.data.data);
+      })
+
+    } catch (err) {
+      console.log(err);
+    }
+  }, [])
+
+
+  return (
+    <div className='App'>
+      <table className='tableAccount'>
+        <tr>
+          <th>Name User</th>
+          <th>Email</th>
+          <th>Sexual</th>
+          <th>Phone number</th>
+        </tr>
+        {data.map((item, index) => (
+          <tr key={index}>
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+            <td>{(item.sex == 0) ? 'Nam' : 'Nữ'}</td>
+            <td>{item.phone}</td>
+            <td>
+              <AiOutlineEdit />
+              <AiOutlineDelete />
+            </td>
+          </tr>
+        )
+        )}
+      </table>
+      {/* <h2>Add a new account : </h2>
+      <form>
+        <input
+          type='text'
+          name='NameUser'
+          required='required'
+          placeholder='Enter a name'
+        />
+        <input
+          type='text'
+          name='Email'
+          required='required'
+          placeholder='Enter a email'
+        />
+        <input
+          type='text'
+          name='Sex'
+          required='required'
+          placeholder='Enter a sexual'
+        />
+        <input
+          type='text'
+          name='PhoneNumber'
+          required='required'
+          placeholder='Enter a phone number'
+        />
+        <button> Add </button>
+      </form> */}
+    </div>
+  );
+};
+export default TableAccount;
