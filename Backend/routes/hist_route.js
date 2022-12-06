f51dc3f24b3fb3f8feffd8cd4e2458cb85dade4b
Minh Hieu <oppahd96@gmail.com>
2022-12-06 16:24:23 +0700

    fix-conflict

--- Backend/routes/hist_route.js
+++ Backend/routes/hist_route.js
@@ -6,4 +6,6 @@ const hist_controller = require("../controllers/hist_controller")
 router.get("/list", hist_controller.getList)
 router.get("/detail", hist_controller.historyDetail)
 router.post("/detailVeh", hist_controller.vehDetail)
+
+
 module.exports = router
